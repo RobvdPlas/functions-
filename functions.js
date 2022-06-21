9b54945ab7fe77b7d564939eb10f5eb7bf254f24
RobvdPlas <robvdplas@outlook.com>
2022-06-21 15:41:12 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -14,20 +14,43 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function school() {
+  return "Novi Hogeschool";
+}
 
+const name = school();
+console.log(name);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function giveCompliment() {
+  return "Lekker bezig met die functies, Jan!";
+}
+
+const compliment = giveCompliment();
+console.log(compliment)
 
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  return [8, 8.5, 6, 7];
+}
 
+const grades = getGrades()
+console.log(grades)
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails () {
+  return {firstName: "jan", lastName: "Janssen"};
+}
+
+const details = getDetails();
+console.log(details);
